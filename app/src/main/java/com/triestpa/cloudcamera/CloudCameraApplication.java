3bc20376515dafc9c17d4107e2e8f974aedc5c64
triestpa <patrick.triest@gmail.com>
2015-12-01 21:27:25 -0500

    Add Picture parse object

--- app/src/main/java/com/triestpa/cloudcamera/CloudCameraApplication.java
+++ app/src/main/java/com/triestpa/cloudcamera/CloudCameraApplication.java
@@ -1,7 +1,9 @@
 package com.triestpa.cloudcamera;
 
 import android.app.Application;
+
 import com.parse.Parse;
+import com.parse.ParseObject;
 
 public class CloudCameraApplication extends Application {
 
@@ -9,6 +11,7 @@ public class CloudCameraApplication extends Application {
     public void onCreate() {
         super.onCreate();
 
+        ParseObject.registerSubclass(Picture.class);
         Parse.initialize(this, "q3IS3BqEI8TLXNxUv05vLlxHfhv7xFICbUdP6xH7", "bFbVdtOaEHH1tcFPLdUWggYzVe6LuCie2bst4SFR");
     }
 }
---
+++ app/src/main/java/com/triestpa/cloudcamera/Picture.java
@@ -0,0 +1,15 @@
+package com.triestpa.cloudcamera;
+
+import com.parse.ParseClassName;
+import com.parse.ParseFile;
+import com.parse.ParseObject;
+
+@ParseClassName("Picture")
+public class Picture extends ParseObject {
+    public ParseFile getPhoto() {
+        return getParseFile("fullSizePhoto");
+    }
+    public void setPhoto(ParseFile photo) {
+        put("fullSizePhoto", photo);
+    }
+}
