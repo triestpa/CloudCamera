5a57765821efec365bc029c9a13d062c41741086
triestpa <patrick.triest@gmail.com>
2015-12-07 15:06:43 -0500

    Store temporary media files in internal cache directory

--- app/src/main/java/com/triestpa/cloudcamera/Utilities/SystemUtilities.java
+++ app/src/main/java/com/triestpa/cloudcamera/Utilities/SystemUtilities.java
@@ -8,7 +8,6 @@ import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
 import android.net.Uri;
 import android.os.Environment;
-import android.util.Log;
 import android.view.Surface;
 import android.view.WindowManager;
 import android.widget.Toast;
@@ -64,57 +63,22 @@ public class SystemUtilities {
    * Create a File for saving an image or video
    */
     public static File getOutputMediaFile(int type) {
-        if (isExternalStorageWritable()) {
-            File mediaStorageDir = new File(Environment.getExternalStoragePublicDirectory(
-                    Environment.DIRECTORY_PICTURES), "CloudCamera");
-            // This location works best if you want the created images to be shared
-            // between applications and persist after your app has been uninstalled.
 
-            // Create the storage directory if it does not exist
-            if (!mediaStorageDir.exists()) {
-                if (!mediaStorageDir.mkdirs()) {
-                    Log.e(TAG, "failed to create directory");
-                    return null;
-                }
-            }
+        File mediaCacheDir = CloudCameraApplication.getAppContext().getCacheDir();
 
-            // Create a media file name
-            String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
-            File mediaFile;
-            if (type == MEDIA_TYPE_IMAGE) {
-                mediaFile = new File(mediaStorageDir.getPath() + File.separator +
-                        "IMG_" + timeStamp + ".jpg");
-            } else if (type == MEDIA_TYPE_VIDEO) {
-                mediaFile = new File(mediaStorageDir.getPath() + File.separator +
-                        "VID_" + timeStamp + ".mp4");
-            } else {
-                return null;
-            }
-            return mediaFile;
-        }
-        else {
-            Log.e(TAG, "External Storage Not Writable");
+        // Create a media file name
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        File mediaFile;
+        if (type == MEDIA_TYPE_IMAGE) {
+            mediaFile = new File(mediaCacheDir.getPath() + File.separator +
+                    "IMG_" + timeStamp + ".jpg");
+        } else if (type == MEDIA_TYPE_VIDEO) {
+            mediaFile = new File(mediaCacheDir.getPath() + File.separator +
+                    "VID_" + timeStamp + ".mp4");
+        } else {
             return null;
         }
-    }
-
-    /* Checks if external storage is available for read and write */
-    public static boolean isExternalStorageWritable() {
-        String state = Environment.getExternalStorageState();
-        if (Environment.MEDIA_MOUNTED.equals(state)) {
-            return true;
-        }
-        return false;
-    }
-
-    /* Checks if external storage is available to at least read */
-    public static boolean isExternalStorageReadable() {
-        String state = Environment.getExternalStorageState();
-        if (Environment.MEDIA_MOUNTED.equals(state) ||
-                Environment.MEDIA_MOUNTED_READ_ONLY.equals(state)) {
-            return true;
-        }
-        return false;
+        return mediaFile;
     }
 
     public static void showToastMessage(String message) {
@@ -129,8 +93,7 @@ public class SystemUtilities {
         // This put the download in the same Download dir the browser uses
         if (mediatype == MEDIA_TYPE_IMAGE) {
             r.setDestinationInExternalPublicDir(Environment.DIRECTORY_DOWNLOADS, id + ".jpeg");
-        }
-        else {
+        } else {
             r.setDestinationInExternalPublicDir(Environment.DIRECTORY_DOWNLOADS, id + ".mp4");
         }
 
